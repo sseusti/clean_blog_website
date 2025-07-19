2693e3ac9b61cbfa04ffdb596e9fa3db460f2fdf
sseusti <stepan.s7ep@gmail.com>
2025-07-20 02:55:53 +0500

    start django project

---
+++ clean_blog/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
+
+def post(request):
+    return render(request, 'blog/post.html')
